382a660ebfd90d69ab4d18b5efa056cb0a8c7432
Kyrill <kyrill@Kyrill-Work.PC>
2015-02-09 15:28:27 +0100

    finallized layout. inserted glcanvas(blue). Linked colorbarnumbers to the slider

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,13 @@ $(function() {
       	values: [ 0, 100 ],
       	min: 0, 
       	max: 200,
+
+      	slide: function( event, ui) {
+      		$( ".colorBarInfoMin" ).val( ui.values[ 0 ]);
+      		$( ".colorBarInfoMax" ).val( ui.values[ 1 ]);
+      		$( ".colorBarInfoMid" ).val( (ui.values[ 0 ] + ui.values[ 1 ])/2);
+
+      	}
     });
 
 
