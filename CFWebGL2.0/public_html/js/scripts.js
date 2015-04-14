ac595987407532a769d11dbc729eff2fd2e5e073
Kyrill <kyrill@Kyrill-Work.PC>
2015-04-14 15:51:50 +0200

    inplemented three.js working on fullscreen with 3d object

--- CFWebGL2.0/public_html/js/scripts.js
+++ CFWebGL2.0/public_html/js/scripts.js
@@ -434,11 +434,10 @@ $(function() {
   /*
     =canvas
   */
-
+    renderingCanvas = $("#renderingCanvas");
     /*
       glcanvas variables
     */
-    var canvas = $("#renderingCanvas");
     
     /*
       canvas full screen
@@ -483,14 +482,69 @@ $(function() {
 
         $("#fullscreenButton").click(function(){
            toggleFullScreen();
-           console.log("make fullscreen");
-        });
+           console.log("make fullscreen"); 
+           onWindowResize();
+       });
         
         
        
     /*
       glcanvas style attributes
     */
+   
+    var canvasWidth = $("#canvasWrap").width();
+    var canvasHeight = $("#canvasWrap").height();
+    var container = $("#container");
+    var renderer = new THREE.WebGLRenderer({antialias: true });
+    renderer.setSize(canvasWidth, canvasHeight);
+    container.append(renderer.domElement);
+
+    var scene = new THREE.Scene;
+
+    var cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+    var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x1ec876 });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.rotation.y = Math.PI * 45 / 180;
+    scene.add(cube);
+
+    var camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 0.1, 10000);
+    camera.position.y = 160;
+    camera.position.z = 400;
+    camera.lookAt(cube.position);
+
+    scene.add(camera);
+
+    var skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
+    var skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.BackSide });
+    var skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
+
+    scene.add(skybox);
+
+    var pointLight = new THREE.PointLight(0xffff00ff);
+    pointLight.position.set(0, 300, 200);
+
+    scene.add(pointLight);
+
+    var clock = new THREE.Clock;
+    
+    
+    function render() {
+            requestAnimationFrame(render);
+
+            cube.rotation.y -= clock.getDelta();
+
+            renderer.render(scene, camera);
+    }
+
+    function onWindowResize() {
+
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            render(); 
+       }
+        
+    render();
 
   /*
   =dropdown
