af0d4740feba81e5a1a82d373f03664cb7e7a0ee
Kyrill <kyrill@Kyrill-Work.PC>
2015-04-29 15:56:50 +0200

    new version almost complete

--- CFWebGL2.0/CFWebGL2.1/src/main/webapp/js/scripts.js
+++ CFWebGL2.0/CFWebGL2.1/src/main/webapp/js/scripts.js
@@ -1,13 +1,332 @@
-var menu = false;
+
+ 
+    var loadingOverlay = $("#loadingOverlay");
+  var body = $("body"); 
+ 
+ document.oncontextmenu = function() {return false;};
+  body.children().css({userSelect: 'none'});
+  /*
+    =data
+  */
+
+    /*
+      data points variables
+    */
+   
+    var points;
+    var distances;
+    var maxDeviation;
+    var minDeviation;
+    var maxDev;
+    var minDev;
+    var numberOfPoints;
+    var absoluteVal = true;
+    var invertSign = false;
+    
+    var consoleNumberOfPoints = $("#consolePoints");
+    var consoleMaxDeviation = $("#consoleDeviation");
+    var consoleLOneNorm = $("#LOneNorm");
+    var consoleLTwoNorm = $("#LTwoNorm");
+    
+    //list of points pulled out of the txt into array
+
+function downloadPoints(){
+    
+    
+        $.ajax({
+           url: "js/autoAlign.txt", // path to file
+           type: "GET",
+           dataType: 'text', // type of file (text, json, xml, etc)
+           success: function(d) { // callback for successful completion
+               distances = d;
+               setDistanceData();
+               
+           }
+         });
+         console.log("downloading");
+         $.ajax({
+           url:  "js/transformed_points.txt", // path to file
+           type: "GET",
+           dataType: 'text', // type of file (text, json, xml, etc)
+           success: function(p) { // callback for successful completion
+               points = p;
+               setPointData();
+               loadAllPoints();
+           }
+         });
+    }
+
+    downloadPoints();
+
+ function setPointData() {
+        
+        points = points.trim();
+        points = points.split(/\s+/g);
+        
+    }
+    
+    function setDistanceData() {
+        distances = distances.trim();
+        distances = distances.split(/\s+/g);
+        
+        for(var i = 0; i < 13; i++) {
+            distances.shift();
+        }
+    
+        numberOfPoints = distances.length;
+        
+        //calculating the minimum and maximum distances
+        minDev = distances[0];
+        maxDev = distances[0];
+        for (var i = 0; i < distances.length; i++) {
+            distances[i] = distances[i] - 0.0;
+            if( distances[i] < minDev)
+               minDev = distances[i];
+            
+            if(distances[i] > maxDev)
+               maxDev = distances[i];
+        }
+                
+        //set number of points console text
+        consoleNumberOfPoints.text(""+ numberOfPoints);
+        //set maximum deviation console text
+        consoleMaxDeviation.text(""+maxDev);
+        //set the l1 norm console text
+        consoleLOneNorm.text(""+ computeL1Norm());
+        //set the l2 norm console text
+        consoleLTwoNorm.text(""+ computeL2Norm());
+        
+    }
+    
+    
+    //calculating the l2 norm
+    function computeL2Norm() {
+        var l2 = 0;
+        for(var i = 0; i < distances.length; i++){
+            var v = distances[i];
+            l2 = l2 + v*v;
+        }
+        var res = Math.sqrt(l2)/distances.length;
+        return res;
+    }
+    
+    //calculating the l1 norm
+    function computeL1Norm() {
+        var m = 0;
+        for(var i = 0; i < distances.length; i++){
+            var v = distances[i];
+            m = m + Math.abs(v);
+        }
+        var res = m/distances.length;
+        return res;
+    }
+    
+    function loadOnlyInside(){
+        absoluteVal = false;
+        invertSign = true;
+        var min = Math.max(0.0,-maxDev);
+        var max = Math.max(0.0,-minDev);
+            
+        setHistogramData(min, max);
+        
+    }
+    
+    function loadAllPoints(){
+        absoluteVal = true;
+        invertSign = false;
+        var min = 0;
+        if (minDev>0) {
+            min = minDev;
+        }
+        var max = Math.max(Math.abs(maxDev), Math.abs(minDev));
+         setHistogramData(min, max);
+         
+    }
+    
+    function loadOnlyOutside(){
+        absoluteVal = false;
+        invertSign = false;
+        var min = 0;
+        var max = maxDev;
+        if (minDev>0) {
+            min = minDev;
+        }
+
+         setHistogramData(min, max);
+    }
+    
+    function loadSignedDist() {
+        absoluteVal = false;
+        invertSign = false;
+        setHistogramData(minDev, maxDev);
+    }
+    
+    function doneLoading(){
+        loadingOverlay.hide();
+        console.log("hide overlay");
+    }
+    
+    function setHistogramData(minDeviation, maxDeviation) {
+        
+        
+    /*
+      =histogram
+    */
+   
+        /*
+            histogram info
+        */
+      
+        
+        var histoDict = [];
+        var bucketSize = 40;
+        var invert = 1.0;
+        
+        if(invertSign) {
+            invert = -1;
+        }
+        
+        //creating an array that will be filled with the buckets
+        for ( var i = 0; i < bucketSize; i++) {    
+            histoDict[i] = 0; 
+        }
+        //setting the tickinterval for the histogram
+        var tickInterval = (maxDeviation - minDeviation)/histoDict.length;
+        
+        //looping through the file and sorting the data into each histogram tick
+        for (var i = 0; i < distances.length; i++) {
+            
+            var v = invert*distances[i];
+            
+            if(absoluteVal){
+                v = Math.abs(v);
+            }
+            
+            if (v < minDeviation || v > maxDeviation) {
+                continue;
+            }
+            
+            for (var j = 0; j < bucketSize; j++) {
+
+                if(v > minDeviation + tickInterval * (j) && v < minDeviation + tickInterval * (j+1) ){
+                    histoDict[j] += 1;
+                    break;
+                } 
+            }
+        }
+            
+     
+    
+        var histogram;
+        
+        //filling the histogram with the sorted data
+        var histogramArray = [[]];
+        
+        for (var k = 0; k < histoDict.length; k++) {
+            
+            histogramArray[0].push([histoDict[k], minDeviation + (tickInterval * k)]);
+        }
+        
+        var histogramOptions = {
+          seriesDefaults: {
+            //making it a bargraph
+            renderer:$.jqplot.BarRenderer,
+
+
+            //renderingoptions for the bars
+            rendererOptions: {
+              barDirection: "horizontal",
+              barWidth: 4,
+              shadow: false,
+              color: "#00ADEF"
+              //color: "#5184c4"
+            }
+          },
+
+          grid:{
+            renderer:$.jqplot.CanvasGridRenderer,
+
+            rendererOptions: {
+                  gridLineColor: "#9E9E9E",
+                  drawBorder: false,
+                  background: "white"
+                }
+          },
+
+          axes:{
+            yaxis:{tickInterval: tickInterval, min: minDeviation, max: maxDeviation},
+            xaxis:{tickInterval: "1", max: 300000},
+
+
+            renderer:$.jqplot.AxisTickRenderer,
+            rendererOptions: {
+
+            }
+          }
+        };
+
+        /*
+          histogram style attributes
+        */  
+       
+        //Filling the histogram with the data set above (location, data, visualisationOptions);
+        
+        histogram = $.jqplot('histogram', histogramArray, histogramOptions);
+        histogram.replot();
+        console.log("done");
+
+    /*        
+    =slider
+    */
+
+        /*
+          slider variables
+        */
+        var slider = $("#slider");
+        var sliderAverageValue = (maxDeviation + minDeviation) / 2;
+
+        Math.ceil(sliderAverageValue);
+
+        /*
+          slider style attributes
+        */  
+
+
+        slider.removeClass('ui-widget').slider({
+            orientation: "vertical",
+            range: true,
+            values: [ minDeviation, maxDeviation ],
+            min: minDeviation, 
+            max: maxDeviation,
+
+            slide: function( event, ui) {
+                $( ".colorBarInfoMin" ).val( ui.values[ 0 ]);
+                $( ".colorBarInfoMax" ).val( ui.values[ 1 ]);
+                $( ".colorBarInfoMid" ).val( (ui.values[ 0 ] + ui.values[ 1 ])/2);
+            }
+        }); 
+
+        $( ".colorBarInfoMin" ).val( minDeviation);
+        $( ".colorBarInfoMax" ).val( maxDeviation);
+        $( ".colorBarInfoMid" ).val( sliderAverageValue);
+
+        doneLoading();
+    }
+
+/*
+    =menu
+*/
+
+var menu = true;
 
 $("#menuToggle").click(function() {
 	if(menu===false) {
-	$("#leftWrap").animate({left: "0"}, {complete: function(){alert: "end ani";}});
-	$("#rightWrap").animate({width: "85%"});
+	$("#leftWrap").animate({left: "0"});
+	$("#rightWrap").animate({width: "85%"},function(){onWindowResize();});
 	menu = true;
 	} else {
 	$("#leftWrap").animate({left: "-13.5%"});
-	$("#rightWrap").animate({width: "98.5%"});
+	$("#rightWrap").animate({width: "98.5%"},function(){onWindowResize();});
 	menu = false;
         
 	}
@@ -18,7 +337,6 @@ $("#instructions").hide();
 /*
     =dropdown
 */
-    
     
         /*
             dropdown variables
@@ -242,7 +560,9 @@ $("#instructions").hide();
     }
     //changing the size of the canvas when the window gets rescaled;
     function onWindowResize() {
-            camera.aspect = window.innerWidth / canvasHeight;
+        canvasWidth = $("#canvasWrap").width();
+     canvasHeight = $("#canvasWrap").height();
+            camera.aspect = canvasWidth / canvasHeight;
             renderer.setSize(canvasWidth, canvasHeight);
             camera.updateProjectionMatrix();
             console.log("resizing window");
